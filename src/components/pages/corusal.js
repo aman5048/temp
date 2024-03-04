7b0545e79545417ea9cf5f03202346ff414abe79
aman5048 <singhaman5048@gmail.com>
2024-03-04 20:19:15 +0530

    initial footer ,navbar and crousal

--- src/components/pages/corusal.js
+++ src/components/pages/corusal.js
@@ -1,23 +1,24 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import { Divide } from "react-feather";
 
 
 function ResponsiveCarousel() {
   const slides=[
     "https://www.creativefabrica.com/wp-content/uploads/2022/06/22/Fashion-Sale-Social-Media-Carousel-Graphics-32166085.png",
-    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
     "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/32acbf101924369.5f2a92935a4ad.jpg"
   ]
   const [idx,setidx]=useState(0);
-  setInterval(()=>{
-    setidx(idx+1);
-    if(idx== slides.length){
-      setidx(0);
-    }
-  },9000)
+  useEffect(()=>{
+    const interval=setInterval(()=>{
+      setidx(idx === (slides.length-1) ? 0:idx+1)
+    },4000);
+    return ()=>clearInterval(interval);
+  },[idx]);
   return ( 
-    <div className="w-full bg-slate-500">
-                    <img src={slides[idx]} className="w-full " alt=""/>
+    <div className="w-full bg-slate-500 xl:h-4/6 md:h-3/6 lg:h-2/6 sm:h-1/4   ">
+      <button>prev</button>
+      <img className="w-full xl:h-full " src={slides[idx]} alt=""/>
+      <button>next</button>
     </div>  
   );
 }
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ResponsiveCarousel from "./corusal";
+import Navbar from "./Navbar";
+import Footer from "./footer";
 
 export default function Home() {
     return (
         <div>
+            <Navbar />
             <ResponsiveCarousel />
+            <Footer/>
         </div>
     );
 }
