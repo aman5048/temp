61169e9cb98fa197242accf3f30f4c8f0874bfd1
aman5048 <singhaman5048@gmail.com>
2024-04-15 15:51:15 +0530

    crousal updated

--- src/components/pages/corusal.js
+++ src/components/pages/corusal.js
@@ -14,26 +14,82 @@ function ResponsiveCarousel() {
     return () => clearInterval(interval);
   }, [idx]);
   return (
-    <div className="w-screen h-screen overflow-hidden">
-      <div className="flex">
-        <button
-          onClick={() => setidx(idx === 0 ? slides.length - 1 : idx - 1)}
-          className="h-full w-12 flex items-center justify-center bg-gray-300 rounded-l-lg"
-        >
-          <Divide size={24} color="black" />
-        </button>
-        <img
-          className="h-full w-full object-cover"
-          style={{ width: "100%", height: "100%", clipPath: "inset(0)" }}
-          src={slides[idx]}
-          alt=""
-        />
-        <button
-          onClick={() => setidx(idx === slides.length - 1 ? 0 : idx + 1)}
-          className="h-full w-12 flex items-center justify-center bg-gray-300 rounded-r-lg"
-        >
-          <Divide size={24} color="black" rotate="180" />
-        </button>
+    // <div className="carousel w-full">
+    //   <div id="slide1" className="carousel-item relative w-full">
+    //     <img
+    //       src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
+    //       className="w-full"
+    //     />
+    // <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    //   <a href="#slide4" className="btn btn-circle">
+    //     ❮
+    //   </a>
+    //   <a href="#slide2" className="btn btn-circle">
+    //     ❯
+    //   </a>
+    // </div>
+    //   </div>
+    //   <div id="slide2" className="carousel-item relative w-full">
+    //     <img
+    //       src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+    //       className="w-full"
+    //     />
+    //     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    //       <a href="#slide1" className="btn btn-circle">
+    //         ❮
+    //       </a>
+    //       <a href="#slide3" className="btn btn-circle">
+    //         ❯
+    //       </a>
+    //     </div>
+    //   </div>
+    //   <div id="slide3" className="carousel-item relative w-full">
+    //     <img
+    //       src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
+    //       className="w-full"
+    //     />
+    //     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    //       <a href="#slide2" className="btn btn-circle">
+    //         ❮
+    //       </a>
+    //       <a href="#slide4" className="btn btn-circle">
+    //         ❯
+    //       </a>
+    //     </div>
+    //   </div>
+    //   <div id="slide4" className="carousel-item relative w-full">
+    //     <img
+    //       src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
+    //       className="w-full"
+    //     />
+    //     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    //       <a href="#slide3" className="btn btn-circle">
+    //         ❮
+    //       </a>
+    //       <a href="#slide1" className="btn btn-circle">
+    //         ❯
+    //       </a>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="w-screen sm:h-1/3 md:h-1/3 lg:h-1/3 xl:h-100  overflow-clip crousel">
+      <div className=" crousel-item relative w-full">
+        <img className="h-full w-full crousel-image" src={slides[idx]} alt="" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a
+            onClick={() => setidx(idx === 0 ? slides.length - 1 : idx - 1)}
+            className="btn btn-circle"
+          >
+            ❮
+          </a>
+          <a
+            onClick={() => setidx(idx === slides.length - 1 ? 0 : idx + 1)}
+            className="btn btn-circle"
+          >
+            ❯
+          </a>
+        </div>
       </div>
     </div>
   );
